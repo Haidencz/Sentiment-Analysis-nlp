9869d36f89bfa3dbc5efc96f660012fed3b6f607
Haidencz <haider.writing@gmail.com>
2026-08-18 16:40:01 +0100

    Added funny images + confidence signifiers

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import joblib
+from PIL import Image
+positive_image = Image.open("assets/positive1.png")
+negative_image = Image.open("assets/negative.png")
 
 model = joblib.load("sentiment_model.pk1")
 vectorizer = joblib.load("tfidf_vectorizer.pk1")
@@ -15,5 +18,7 @@ if st.button("Analyse"):
         confidence = max(probabilities) * 100
         if prediction == 1:
             st.success(f"positive review {confidence: .1f}% confidence")
+            st.image(positive_image, caption="positive!!!", width=200)
         else:
             st.error(f"negative review {confidence: .1f}% confidence")
+            st.image(negative_image, caption="negative!!!", width=200)
