0ed662f13e8bc5f597741564928da68eb9f596bd
Haidencz <haider.writing@gmail.com>
2026-08-18 16:18:14 +0100

    Handle empty inputs

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ vectorizer = joblib.load("tfidf_vectorizer.pk1")
 st.title("Movie Review sentiment Analyser")
 review = st.text_area("enter a movie review")
 if st.button("Analyse"):
-    review_vector = vectorizer.transform([review])
-    probabilities = model.predict_proba(review_vector)[0]
-    prediction = model.predict(review_vector)
-    confidence = max(probabilities) * 100
-    if prediction == 1:
-        st.success(f"positive review {confidence: .1f}% confidence")
+    if not review.strip():
+        st.warning("please enter a review first.")
     else:
-        st.error(f"negative review {confidence: .1f}% confidence")
+        review_vector = vectorizer.transform([review])
+        probabilities = model.predict_proba(review_vector)[0]
+        prediction = model.predict(review_vector)
+        confidence = max(probabilities) * 100
+        if prediction == 1:
+            st.success(f"positive review {confidence: .1f}% confidence")
+        else:
+            st.error(f"negative review {confidence: .1f}% confidence")
