d6d6531db971bbdcc5624ce101a7b316ef9e072b
Haidencz <haider.writing@gmail.com>
2026-08-18 14:46:50 +0100

    convert dataset using TFIDF features

--- Sentimnt_model.py
+++ Sentimnt_model.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 data_path = Path("data/aclImdb/train")
 positive_path = data_path/"pos"
 negative_path = data_path / "neg"
@@ -36,6 +38,12 @@ print(df["sentiment"].value_counts())
 X = df["review"]
 y = df["sentiment"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+vectorizer = TfidfVectorizer() #create vectorizer
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+print(X_train_tfidf.shape)
+print(X_test_tfidf.shape)
+
 print("training reviews:", len(X_train)) #20k
 print("test reviews:", len(X_test)) #5000
 
